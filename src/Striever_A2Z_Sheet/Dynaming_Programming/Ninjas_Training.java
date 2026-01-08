55112826eccf64f53c0ab6b2b8cf3f59b5e21e9c
Sumit Kumar <developer.sumit.professional@gmail.com>
2026-01-08 17:33:31 +0530

    Test1

--- src/Striever_A2Z_Sheet/Dynaming_Programming/Ninjas_Training.java
+++ src/Striever_A2Z_Sheet/Dynaming_Programming/Ninjas_Training.java
@@ -3,5 +3,6 @@ package Striever_A2Z_Sheet.Dynaming_Programming;
 public class Ninjas_Training {
     public static void main(String[] args) {
         System.out.println("hellos");
+        System.out.println("hello");
     }
 }
