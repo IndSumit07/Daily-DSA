4bac1ae895f185f09420de82dff050b653c607e4
Sumit Kumar <developer.sumit.professional@gmail.com>
2026-01-08 09:45:35 +0530

    Test 1

--- src/GFG/Day_08.java
+++ src/GFG/Day_08.java
@@ -1,5 +1,10 @@
 package GFG;
 
+/*
+    You are given an array arr[] of positive integers and an integer k. You
+    have to count the number of subarrays that contain exactly k odd numbers.
+*/
+
 public class Day_08 {
     public static void main(String[] args) {
 
---
+++ src/Leetcode/Day_08.java
@@ -0,0 +1,7 @@
+package Leetcode;
+
+public class Day_08 {
+    public static void main(String[] args) {
+
+    }
+}
